fe5cfc4a944a3afdfd659fc2bf0719d5d71b803d
Nucly <nucly@proton.me>
2025-04-22 13:06:20 +0700

    refactoring

---
+++ command_help.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n")
+	for _, command := range initialiseCommands() {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
--- repl.go
+++ repl.go
@@ -1,10 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := initialiseCommands()
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		command := cleanInput(text)
+
+		if len(command) == 0 {
+			continue
+		}
+
+		if value, ok := commands[command[0]]; ok {
+			err := value.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+	}
+}
 
 type cliCommand struct {
 	name        string
@@ -19,16 +47,15 @@ func initialiseCommands() map[string]cliCommand {
 			description: "Exit the pokedex",
 			callback:    commandExit,
 		},
+		"help": {
+			name:        "help",
+			description: "Display a help message",
+			callback:    commandHelp,
+		},
 	}
 }
 
 func cleanInput(text string) []string {
 	lower := strings.ToLower(text)
 	return strings.Fields(lower)
-}
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
 }
